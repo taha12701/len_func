0371e6ee078a7b5e64c91b49275b982b25b462a9
taha12701 <108870371+taha12701@users.noreply.github.com>
2024-07-09 17:02:38 +0500

    Add files via upload

---
+++ len_func.py
@@ -0,0 +1,21 @@
+# write a program to find the length of the list
+
+# list1=["Babar" , "shadab" , " bumrah" , "kohli" , "rohit"]
+cities=["Lahore","Karachi","Sialkot", "Faisalabad", "Multan", "Ziarat"]
+
+# def cal_list(list):
+#   print(len(list))
+  
+
+# cal_list(list1)
+# cal_list(cities)
+
+# def calc_len(list):
+#   print(len(list), end="\n")
+
+# def print_cities(list):
+#   for item in list:
+#     print(item, end=" ")
+
+# print_cities(cities)
+# calc_len(cities)
